4fa68b17d651d1b812ab48eafbb1e1757352d5db
Carlos Martinez <carlos0492@outlook.com>
2022-03-09 11:37:38 -0500

    change chart

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,76 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>Using VueJs for Bigfix web reports and dashboards</p>
+    <button @click="increment">Count: {{ count }}</button>
+    <button @click="nowRelevance">Now Relevance</button>
+    <p>{{ relevance }}</p>
+    <p>Demo chart:</p>
+    <div id="chart-01">
+    <apexchart
+      width="500"
+      type="bar"
+      :options="chartOptions"
+      :series="series"
+    ></apexchart>
+  </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from "vue";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+   setup() {
+    const count = ref(0);
+    const relevance = ref("");
+
+    const chartOptions = {
+
+      chart: {
+        id: "basic-bar"
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      },
+      yaxis: {
+        title: {
+          text: "Y-Axis"
+        }
+      }
+    };
+
+    const series = [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }
+    ];
+
+    const increment = () => {
+      count.value++;
+      if (typeof window.EvaluateRelevance !== "undefined") {
+      }
+    };
+
+    const nowRelevance = () => {
+      if (typeof window.EvaluateRelevance !== "undefined") {
+        relevance.value = window.EvaluateRelevance("now");
+      } else {
+        relevance.value = "none";
+      }
+    };
+
+    return {
+      count,
+      relevance,
+      chartOptions,
+      series,
+      increment,
+      nowRelevance,
+    };
   },
 };
 </script>
@@ -22,5 +83,27 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+#chart-01 {
+ margin: auto;
+  width: 50%;
+
+  padding: 10px;
 }
 </style>
--- src/components/Chart.vue
+++
@@ -1,7 +0,0 @@
-<template>
-<div>
- <apexchart width="380" type="donut" :options="options" :series="series"></apexchart>
-</div>
-</template>
-
-<script></script>
